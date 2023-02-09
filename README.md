cdb33c591cc13fd388315e19d6fdaaaf022c0603
Vallarasu <valla@gmail.com>
2023-02-09 10:44:54 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Login Api With Password Credentials
---
+++ UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Car_Workshop_WebAPI
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
